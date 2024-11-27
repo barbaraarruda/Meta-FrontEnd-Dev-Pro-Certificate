412b3a50b546b1c5ca38cf2cfbae934a2df9c23f
Bárbara Arruda <65787282+barbaraarruda@users.noreply.github.com>
2024-11-27 14:17:01 -0300

    exercise 01 variables

---
+++ Programming With JavaScript/Variables/exercise01Variables.js
@@ -0,0 +1,10 @@
+var petCat = "Pepper";
+var petDog = "Rex";
+console.log(petCat);
+console.log(petDog);
+console.log("My pet dog`s name is: ", petDog);
+console.log("My pet cat`s name is: ", petCat);
+var catSound = "purr";
+var dogSound = "woof";
+console.log(petDog, "says", dogSound);
+console.log(petCat, "says", catSound);
